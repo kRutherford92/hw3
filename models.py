97ca01cf902a239cea99cff46d5e630e6b23f2c0
Kristen Rutherford <kmr4@rice.edu>
2018-10-18 16:03:05 -0500

    base works

--- models.py
+++ models.py
@@ -4,14 +4,38 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
-	__tablename__ = 'users'
-	uid = db.Column(db.Integer, primary_key=True, autoincrement=True)
-	username = db.Column(db.String(64), unique=True, nullable=False)
-	password = db.Column(db.String(128), nullable=False)
+    __tablename__ = 'users'
+    uid = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+
+    def is_following(self, user):
+        return self.followed.filter(follows.c.following == user.uid).count() > 0
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def followed_posts(self):
+        followed_posts = Post.query.join(follows,
+            (follows.c.following == Post.author)).filter(follows.c.follower == self.uid)
+        self_posts = Post.query.filter_by(author=self.uid)
+        return followed_posts.union(self_posts)        
+    
 
 class Post(db.Model):
-	__tablename__ = 'posts'
-	pid = db.Column(db.Integer, primary_key=True, autoincrement=True)
-	author = db.Column(db.Integer, db.ForeignKey('users.uid'), nullable=False)
-	content = db.Column(db.String(1024), nullable=False)
-		
+    __tablename__ = 'posts'
+    pid = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    author = db.Column(db.Integer, db.ForeignKey('users.uid'), nullable=False)
+    content = db.Column(db.String(1024), nullable=False)    
+
+
+class Follows(db.Model):
+	__tablename__ = 'follows'
+	fid = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	follower = db.Column(db.Integer, db.ForeignKey('users.uid'), nullable=False)
+	following = db.Column(db.Integer, db.ForeignKey('users.uid'), nullable=False)		
--- routes.py
+++ routes.py
@@ -1,15 +1,15 @@
 #import libraries
 from flask import Flask, flash, render_template, request, url_for, redirect, jsonify, session
 from passlib.hash import sha256_crypt
-from models import db, User, Post
+from models import db, User, Post, Follows
 from forms import LoginForm, SignupForm, NewpostForm
 from flask_heroku import Heroku
 
 app = Flask(__name__)
 app.secret_key = "cscie14a-hw3"
 
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Winchester110283@localhost:5432/hw3_db' 
-heroku = Heroku(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Winchester110283@localhost:5432/hw3_db' 
+#heroku = Heroku(app)
 
 db.init_app(app)
 
@@ -19,8 +19,12 @@ db.init_app(app)
 def index():
 	if 'username' in session:
 		session_user = User.query.filter_by(username=session['username']).first()
-		posts = Post.query.filter_by(author=session_user.uid).all()
-		return render_template('index.html', title='Home', posts=posts, session_username=session_user.username)
+		#posts = Post.query.filter_by(author=session_user.uid).all()
+		users_followed = Follows.query.filter_by(follower=session_user.uid).all()
+		uids_followed = [f.following for f in users_followed] + [session_user.uid]
+		followed_posts = Post.query.filter(Post.author.in_(uids_followed)).all()
+
+		return render_template('index.html', title='Home', posts=followed_posts, session_username=session_user.username)
 	else:
 		all_posts = Post.query.all()
 		return render_template('index.html', title='Home', posts=all_posts)
@@ -86,6 +90,45 @@ def signup():
 			return redirect(url_for('login'))
 	else:
 		return render_template('signup.html', title='Signup', form=form)
+
+@app.route('/search', methods=['POST'])
+def search():
+	user_to_query = request.form['search_box']
+	return redirect(url_for('profile', username=user_to_query))
+
+@app.route('/profile/<username>', methods=['GET'])
+def profile(username):
+	profile_user = User.query.filter_by(username=username).first()
+	profile_user_posts = Post.query.filter_by(author=profile_user.uid).all()
+
+	if "username" in session:
+		session_user = User.query.filter_by(username=session['username']).first()
+		if Follows.query.filter_by(follower=session_user.uid, following=profile_user.uid).first():
+			followed = True
+		else:
+			followed = False
+		return render_template('profile.html', user=profile_user, posts=profile_user_posts, followed=followed)
+	return render_template('profile.html', user=profile_user, posts=profile_user_posts)
+
+@app.route('/follow/<username>', methods=['POST'])
+def follow(username):
+	session_user = User.query.filter_by(username=session['username']).first()
+	user_to_follow = User.query.filter_by(username=username).first()
+	new_follow = Follows(follower=session_user.uid, following=user_to_follow.uid)
+
+	db.session.add(new_follow)
+	db.session.commit()
+	return redirect(url_for('profile', username=username))
+
+@app.route('/unfollow/<username>', methods=['POST'])
+def unfollow(username):
+	session_user = User.query.filter_by(username=session['username']).first()
+	user_to_unfollow = User.query.filter_by(username=username).first()
+
+	delete_follow = Follows.query.filter_by(follower=session_user.uid, following=user_to_unfollow.uid).first()
+	db.session.delete(delete_follow)
+	db.session.commit()
+	return redirect(url_for('profile', username=username))
 
 if __name__ == "__main__":
     app.run(debug=True)
